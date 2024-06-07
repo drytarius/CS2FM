41d83d32a5d4ee492212f926c2943278311a3500
dt123 <bugro120@gmail.com>
2024-06-08 00:02:51 +0300

    First Commit
    First commit, added project files.

---
+++ CS2FM/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CS2FM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.corebtn = new System.Windows.Forms.Button();
+            this.gamebtn = new System.Windows.Forms.Button();
+            this.corepath = new System.Windows.Forms.Label();
+            this.gamepath = new System.Windows.Forms.Label();
+            this.fontlist = new System.Windows.Forms.ListBox();
+            this.addfontbtn = new System.Windows.Forms.Button();
+            this.removefontbtn = new System.Windows.Forms.Button();
+            this.applyfontbtn = new System.Windows.Forms.Button();
+            this.directorygroup = new System.Windows.Forms.GroupBox();
+            this.gamedirlabel = new System.Windows.Forms.Label();
+            this.coredirlabel = new System.Windows.Forms.Label();
+            this.upbtn = new System.Windows.Forms.Button();
+            this.downbtn = new System.Windows.Forms.Button();
+            this.directorygroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // corebtn
+            // 
+            this.corebtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.corebtn.Location = new System.Drawing.Point(149, 208);
+            this.corebtn.Name = "corebtn";
+            this.corebtn.Size = new System.Drawing.Size(84, 20);
+            this.corebtn.TabIndex = 1;
+            this.corebtn.Text = "Select Core Dir";
+            this.corebtn.UseVisualStyleBackColor = true;
+            this.corebtn.Click += new System.EventHandler(this.corebtn_Click);
+            // 
+            // gamebtn
+            // 
+            this.gamebtn.Location = new System.Drawing.Point(243, 208);
+            this.gamebtn.Name = "gamebtn";
+            this.gamebtn.Size = new System.Drawing.Size(84, 20);
+            this.gamebtn.TabIndex = 2;
+            this.gamebtn.Text = "Select Game Dir";
+            this.gamebtn.UseVisualStyleBackColor = true;
+            this.gamebtn.Click += new System.EventHandler(this.gamebtn_Click);
+            // 
+            // corepath
+            // 
+            this.corepath.AutoEllipsis = true;
+            this.corepath.AutoSize = true;
+            this.corepath.Location = new System.Drawing.Point(5, 46);
+            this.corepath.Name = "corepath";
+            this.corepath.Size = new System.Drawing.Size(16, 13);
+            this.corepath.TabIndex = 3;
+            this.corepath.Text = "...";
+            // 
+            // gamepath
+            // 
+            this.gamepath.AutoEllipsis = true;
+            this.gamepath.AutoSize = true;
+            this.gamepath.Location = new System.Drawing.Point(5, 84);
+            this.gamepath.Name = "gamepath";
+            this.gamepath.Size = new System.Drawing.Size(16, 13);
+            this.gamepath.TabIndex = 4;
+            this.gamepath.Text = "...";
+            // 
+            // fontlist
+            // 
+            this.fontlist.FormattingEnabled = true;
+            this.fontlist.Location = new System.Drawing.Point(10, 10);
+            this.fontlist.Name = "fontlist";
+            this.fontlist.Size = new System.Drawing.Size(129, 160);
+            this.fontlist.TabIndex = 5;
+            // 
+            // addfontbtn
+            // 
+            this.addfontbtn.Location = new System.Drawing.Point(12, 175);
+            this.addfontbtn.Name = "addfontbtn";
+            this.addfontbtn.Size = new System.Drawing.Size(39, 20);
+            this.addfontbtn.TabIndex = 6;
+            this.addfontbtn.Text = "Add";
+            this.addfontbtn.UseVisualStyleBackColor = true;
+            this.addfontbtn.Click += new System.EventHandler(this.addfontbtn_Click);
+            // 
+            // removefontbtn
+            // 
+            this.removefontbtn.Location = new System.Drawing.Point(51, 175);
+            this.removefontbtn.Name = "removefontbtn";
+            this.removefontbtn.Size = new System.Drawing.Size(39, 20);
+            this.removefontbtn.TabIndex = 7;
+            this.removefontbtn.Text = "Rem";
+            this.removefontbtn.UseVisualStyleBackColor = true;
+            this.removefontbtn.Click += new System.EventHandler(this.removefontbtn_Click);
+            // 
+            // applyfontbtn
+            // 
+            this.applyfontbtn.Location = new System.Drawing.Point(12, 237);
+            this.applyfontbtn.Name = "applyfontbtn";
+            this.applyfontbtn.Size = new System.Drawing.Size(315, 28);
+            this.applyfontbtn.TabIndex = 8;
+            this.applyfontbtn.Text = "Apply Selected Font";
+            this.applyfontbtn.UseVisualStyleBackColor = true;
+            this.applyfontbtn.Click += new System.EventHandler(this.applyfontbtn_Click);
+            // 
+            // directorygroup
+            // 
+            this.directorygroup.Controls.Add(this.gamedirlabel);
+            this.directorygroup.Controls.Add(this.coredirlabel);
+            this.directorygroup.Controls.Add(this.corepath);
+            this.directorygroup.Controls.Add(this.gamepath);
+            this.directorygroup.Location = new System.Drawing.Point(144, 12);
+            this.directorygroup.Name = "directorygroup";
+            this.directorygroup.Size = new System.Drawing.Size(183, 191);
+            this.directorygroup.TabIndex = 9;
+            this.directorygroup.TabStop = false;
+            this.directorygroup.Text = "Directories:";
+            // 
+            // gamedirlabel
+            // 
+            this.gamedirlabel.AutoSize = true;
+            this.gamedirlabel.Location = new System.Drawing.Point(5, 71);
+            this.gamedirlabel.Name = "gamedirlabel";
+            this.gamedirlabel.Size = new System.Drawing.Size(83, 13);
+            this.gamedirlabel.TabIndex = 5;
+            this.gamedirlabel.Text = "Game Directory:";
+            // 
+            // coredirlabel
+            // 
+            this.coredirlabel.AutoSize = true;
+            this.coredirlabel.Location = new System.Drawing.Point(5, 33);
+            this.coredirlabel.Name = "coredirlabel";
+            this.coredirlabel.Size = new System.Drawing.Size(77, 13);
+            this.coredirlabel.TabIndex = 4;
+            this.coredirlabel.Text = "Core Directory:";
+            // 
+            // upbtn
+            // 
+            this.upbtn.Image = global::CS2FM.Properties.Resources.ico_up;
+            this.upbtn.Location = new System.Drawing.Point(96, 175);
+            this.upbtn.Name = "upbtn";
+            this.upbtn.Size = new System.Drawing.Size(21, 20);
+            this.upbtn.TabIndex = 10;
+            this.upbtn.UseVisualStyleBackColor = true;
+            this.upbtn.Click += new System.EventHandler(this.upbtn_Click);
+            // 
+            // downbtn
+            // 
+            this.downbtn.Image = global::CS2FM.Properties.Resources.ico_down;
+            this.downbtn.Location = new System.Drawing.Point(118, 175);
+            this.downbtn.Name = "downbtn";
+            this.downbtn.Size = new System.Drawing.Size(21, 20);
+            this.downbtn.TabIndex = 11;
+            this.downbtn.UseVisualStyleBackColor = true;
+            this.downbtn.Click += new System.EventHandler(this.downbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 276);
+            this.Controls.Add(this.downbtn);
+            this.Controls.Add(this.upbtn);
+            this.Controls.Add(this.directorygroup);
+            this.Controls.Add(this.applyfontbtn);
+            this.Controls.Add(this.corebtn);
+            this.Controls.Add(this.removefontbtn);
+            this.Controls.Add(this.addfontbtn);
+            this.Controls.Add(this.fontlist);
+            this.Controls.Add(this.gamebtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CS2FM";
+            this.directorygroup.ResumeLayout(false);
+            this.directorygroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Button corebtn;
+        private Button gamebtn;
+        private Label corepath;
+        private Label gamepath;
+        private ListBox fontlist;
+        private Button addfontbtn;
+        private Button removefontbtn;
+        private Button applyfontbtn;
+        private GroupBox directorygroup;
+        private Label coredirlabel;
+        private Label gamedirlabel;
+        private Button upbtn;
+        private Button downbtn;
+    }
+}
---
+++ CS2FM/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Text;
+using System.Text;
+
+namespace CS2FM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+
+
+        //--[[---------------------------------------------------------
+        //	Name: Save, load, read, fetch fonts.
+        //	Desc: Self explanatory.
+        //-----------------------------------------------------------]]
+
+        private void LoadSettings()
+        {
+            corepath.Text = Settings.Default.CoreFilePath;
+            gamepath.Text = Settings.Default.GameFilePath;
+
+            // Initialize font list
+            fontlist.Items.Clear();
+
+            // Load font details from settings
+            StringCollection serializedFontDetails = Settings.Default.FontList;
+            if (serializedFontDetails != null)
+            {
+                foreach (string serializedFont in serializedFontDetails)
+                {
+                    // Deserialize the serialized font details string into a FontDetails object
+                    string[] fontParts = serializedFont.Split(',');
+                    if (fontParts.Length == 3)
+                    {
+                        FontDetails fontDetails = new FontDetails(fontParts[0], fontParts[1], fontParts[2]);
+                        fontlist.Items.Add(fontDetails);
+                    }
+                }
+            }
+        }
+
+        private void SaveSettings()
+        {
+            Settings.Default.CoreFilePath = corepath.Text;
+            Settings.Default.GameFilePath = gamepath.Text;
+
+            // Initialize a new StringCollection for font details
+            StringCollection serializedFontDetails = new StringCollection();
+            foreach (FontDetails fontDetails in fontlist.Items)
+            {
+                // Serialize FontDetails object into a string and add it to the StringCollection
+                string serializedFont = $"{fontDetails.Path},{fontDetails.FileName},{fontDetails.FontName}";
+                serializedFontDetails.Add(serializedFont);
+            }
+
+            // Save serialized font details to settings
+            Settings.Default.FontList = serializedFontDetails;
+            Settings.Default.Save();
+        }
+
+        private string GetFontName(string filePath)
+        {
+            // Load the font file
+            using (PrivateFontCollection privateFontCollection = new PrivateFontCollection())
+            {
+                privateFontCollection.AddFontFile(filePath);
+
+                // Get the font family name
+                if (privateFontCollection.Families.Length > 0)
+                {
+                    return privateFontCollection.Families[0].Name;
+                }
+            }
+
+            return null;
+        }
+
+        // Define a class to hold font details
+        public class FontDetails
+        {
+            public string Path { get; }
+            public string FileName { get; }
+            public string FontName { get; }
+
+            public FontDetails(string path, string fileName, string fontName)
+            {
+                Path = path;
+                FileName = fileName;
+                FontName = fontName;
+            }
+
+            public override string ToString()
+            {
+                return $"{FontName} ({FileName}) - {Path}";
+            }
+        }
+
+
+
+        //--[[---------------------------------------------------------
+        //	Name: Select directories and display the selected directory as label content.
+        //	Desc: Self explanatory.
+        //-----------------------------------------------------------]]
+
+        private void corebtn_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select Core Directory";
+
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                    corepath.Text = selectedFolderPath;
+                    SaveSettings();
+                }
+            }
+        }
+
+        private void gamebtn_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select Game Directory";
+
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    string selectedFolderPath = folderBrowserDialog.SelectedPath;
+                    gamepath.Text = selectedFolderPath;
+                    SaveSettings();
+                }
+            }
+        }
+
+
+
+        //--[[---------------------------------------------------------
+        //	Name: Add or remove fonts.
+        //	Desc: Set of buttons.
+        //-----------------------------------------------------------]]
+
+        private void addfontbtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Font Files (*.ttf, *.otf, *.fon, *.fnt, *.ttc, *.woff, *.woff2, *.eot)|*.ttf;*.otf;*.fon;*.fnt;*.ttc;*.woff;*.woff2;*.eot";
+                openFileDialog.Multiselect = true;
+                openFileDialog.Title = "Add Font File(s)";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (string filePath in openFileDialog.FileNames)
+                    {
+                        // Extract font name from font file
+                        string fontName = GetFontName(filePath);
+
+                        // Add font details to the font list
+                        fontlist.Items.Add(new FontDetails(filePath, Path.GetFileName(filePath), fontName));
+                    }
+
+                    SaveSettings();
+                }
+            }
+        }
+
+        private void removefontbtn_Click(object sender, EventArgs e)
+        {
+            // Remove selected font from the list
+            if (fontlist.SelectedItem != null)
+            {
+                fontlist.Items.Remove(fontlist.SelectedItem);
+                SaveSettings();
+            }
+            else
+            {
+                MessageBox.Show("Please select a font to remove from the list.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+
+        //--[[---------------------------------------------------------
+        //	Name: Move selected font list item.
+        //	Desc: Moves the selected font in the list up or down.
+        //-----------------------------------------------------------]]
+
+        private void upbtn_Click(object sender, EventArgs e)
+        {
+            MoveItem(-1);
+        }
+
+        private void downbtn_Click(object sender, EventArgs e)
+        {
+            MoveItem(1);
+        }
+
+        private void MoveItem(int direction)
+        {
+            // Check if an item is selected and it's not the first or last item
+            if (fontlist.SelectedItem != null && fontlist.SelectedIndex >= 0 && fontlist.SelectedIndex < fontlist.Items.Count)
+            {
+                // Get the index of the selected item
+                int index = fontlist.SelectedIndex;
+
+                // Calculate the new index after moving the item
+                int newIndex = index + direction;
+
+                // Ensure the new index is within bounds
+                if (newIndex >= 0 && newIndex < fontlist.Items.Count)
+                {
+                    // Remove the selected item from the ListBox
+                    object selectedItem = fontlist.SelectedItem;
+                    fontlist.Items.Remove(selectedItem);
+
+                    // Insert the item at the new position
+                    fontlist.Items.Insert(newIndex, selectedItem);
+
+                    // Select the item at the new position
+                    fontlist.SetSelected(newIndex, true);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //--[[---------------------------------------------------------
+        //	Name: Apply selected font.
+        //	Desc: Searches for the default "stratum2.uifont" deletes and replaces it with the selected font.
+        //  Replaces the "font.conf" and "42-repl-global.conf" accordingly.
+        //-----------------------------------------------------------]]
+
+        private void applyfontbtn_Click(object sender, EventArgs e)
+        {
+            if (fontlist.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a font from the list.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Get the selected font details
+            FontDetails selectedFont = (FontDetails)fontlist.SelectedItem;
+
+            // Read the content of the template fonts.conf file from resources as byte[]
+            byte[] templateFontsConfBytes = Properties.Resources.fonts_conf;
+            byte[] template42ReplGlobalConfBytes = Properties.Resources.global;
+
+            try
+            {
+                // Convert byte[] content to string
+                string templateFontsConfContent = Encoding.UTF8.GetString(templateFontsConfBytes);
+                string template42ReplGlobalConfContent = Encoding.UTF8.GetString(template42ReplGlobalConfBytes);
+
+                // Replace placeholders with selected font details
+                templateFontsConfContent = templateFontsConfContent.Replace("FONTFILENAME", selectedFont.FileName);
+                templateFontsConfContent = templateFontsConfContent.Replace("FONTNAME", selectedFont.FontName);
+
+                // Replace placeholders with selected font details in 42-repl-global.conf
+                template42ReplGlobalConfContent = template42ReplGlobalConfContent.Replace("FONTFILENAME", selectedFont.FileName);
+                template42ReplGlobalConfContent = template42ReplGlobalConfContent.Replace("FONTNAME", selectedFont.FontName);
+
+                // Get the game and core directory paths
+                string gameDirectory = gamepath.Text;
+                string coreDirectory = corepath.Text;
+
+                string selectedFontPath = selectedFont.Path;
+
+                string stratum2FontPath = Path.Combine(gameDirectory, "stratum2.uifont");
+                string fontsConfPath = Path.Combine(gameDirectory, "fonts.conf");
+                string replGlobalConfPath = Path.Combine(coreDirectory, "42-repl-global.conf");
+
+
+                // Delete existing stratum2.uifont file if it exists
+                if (File.Exists(stratum2FontPath))
+                {
+                    File.Delete(stratum2FontPath);
+                }
+
+                // Write the modified content to the fonts.conf file in the game directory
+                File.WriteAllText(fontsConfPath, templateFontsConfContent);
+
+                // Write the modified content to the 42-repl-global.conf file in the core directory
+                File.WriteAllText(replGlobalConfPath, template42ReplGlobalConfContent);
+
+                // Copy the selected font to the game directory
+                string selectedFontFileName = Path.GetFileName(selectedFontPath);
+                string destinationPath = Path.Combine(gameDirectory, selectedFontFileName);
+                File.Copy(selectedFontPath, destinationPath, true); // The 'true' parameter allows overwriting if the file already exists
+                MessageBox.Show("Font applied successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while applying the font: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
